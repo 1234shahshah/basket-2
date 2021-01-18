32836df7f52c2992288bbd249382cc6332e535db
1234shahshah <71225198+1234shahshah@users.noreply.github.com>
2021-01-18 11:57:22 -0600

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -56,14 +56,16 @@ class Game{
                      players[index - 1].y = y;
                        
                      if(index === player.index){
-                         
-                         // to display player name on the basket.
-
+                              fill("black")
+                           textSize(25)
+                           text(allPlayers[plr].name,x-25,y+25)                                  
                          
                      }
                     
-                     //text to display player score.
-                 
+                     textSize(25)
+                     fill("red")
+                     text("player1:"+allPlayers.player1.score,50,50)
+                     text("player2:"+allPlayers.player2.score,50,100)
                  }
                 
                 
@@ -99,8 +101,16 @@ class Game{
                  }
                  
                   if (player.index !== null) {
-                     //fill code here, to destroy the objects. (Use the one in the class project 39)
-                     // add the condition to calculate the score. and use update ti update the values in the database.
+                      for(var i = 0; i<fruitGroup.length;i++){
+
+                      
+                    if (fruitGroup.get(i).isTouching (players))
+                    {
+                        fruitGroup.get(i).destroy()
+                        player.score=player.score+1
+                        player.update()
+                      } 
+                    }
                   }
                 
 
